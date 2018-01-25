b25d4069fd47d7f60c0500085b7eb9437f887522
DanielDwyer <danielpatrickdwyer@gmail.com>
2018-01-25 08:31:39 -0700

    updates to bin

--- bin/news.js
+++ bin/news.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-// var lib = require('../lib/index.js');
-// var news = lib.terminalTimes();
-//
-// console.log(news);
+var lib = require('../lib/index.js');
+var news = lib.terminalTimes();
+
+console.log(news);
